5b1bdd8d025f5a9fe4e1b324589809cd6a4707b1
killsos <killsos.ql@gmail.com>
2017-04-04 19:33:12 +0800

    956

--- 01.js
+++ 01.js
@@ -1,9 +1,11 @@
-function partent(){
+function parent(){
   function son(){
 
   }
 }
 
-console.log(Windows);
+console.log(window);
 
-console.log(Window.son);
+console.log(window.parent); // 最外面是全局对象的一个属性
+
+console.log(window.son);    // 函数里面函数不是
---
+++ 02.js
@@ -0,0 +1,11 @@
+son();
+
+function parent(){
+
+  console.log("parent")
+
+  function son(){
+    console.log("son")
+  }
+
+}
